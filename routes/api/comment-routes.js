7987ea9695836650eb3c60203db7416175fb45ab
kea6t <keasay26@gmail.com>
2022-08-20 19:02:53 -0400

    Update the Routes to Include Comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
+    
+});
+
+router.post('/', (req, res) => {
     Comment.create({
         comment_text: req.body.comment_text,
         user_id: req.body.user_id,
@@ -12,10 +16,6 @@ router.get('/', (req, res) => {
           console.log(err);
           res.status(400).json(err);
         });
-});
-
-router.post('/', (req, res) => {
-
 });
 
 router.delete('/:id', (req, res) => {

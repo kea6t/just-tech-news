3b40b8fd16413cfee433518804b90b29cb1896f3
kea6t <keasay26@gmail.com>
2022-08-24 13:30:21 -0400

    changed some connection.js name convention

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     // create connection to our database, pass in your MySql information for username and password
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
